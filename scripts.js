038bea2e2819d8d399c2a2a04e3e30d175cefe64
gregoriB <brandon.gregori@gmail.com>
2019-02-02 14:34:07 -0800

    added wiki page compatibility in a new "wiki.css" file and also in scripts.js "fixSiteTableWidth()".  Fixed comment styling in the "gilded" section

--- scripts.js
+++ scripts.js
@@ -1,14 +1,26 @@
 const sidePanel = document.querySelector('.side');
 const linklisting = document.querySelector('.linklisting');
+const wiki = document.querySelector('.wiki-page');
+const content = document.querySelector('div.content')
 
 // fixes issue with some subs having threads that overlap the side panel
 const fixSiteTableWidth = () => {
   const extraGap = 15;
-  linklisting.setAttribute('style', (
-    `max-width: ${window.innerWidth - sidePanel.offsetWidth}px !important`,
-    `margin-right: ${sidePanel.offsetWidth + extraGap}px`
-    )
-  );
+  if (linklisting) {
+    content.setAttribute('style', 'margin-right: 0 !important');
+    linklisting.setAttribute('style', (
+      `max-width: ${window.innerWidth - sidePanel.offsetWidth}px !important`,
+      `margin-right: ${sidePanel.offsetWidth + extraGap}px`
+      )
+    );
+  }
+  if (wiki) {
+      content.setAttribute('style', (
+      `max-width: ${window.innerWidth - sidePanel.offsetWidth}px !important`,
+      `margin-right: ${sidePanel.offsetWidth + extraGap}px`
+      )
+    );
+  }
 }
 
 fixSiteTableWidth();
