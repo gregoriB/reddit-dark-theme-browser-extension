92aff816dc2d303a54be56033b5e1b10ad6d6232
gregoriB <brandon.gregori@gmail.com>
2019-02-02 11:40:55 -0800

    updated .linklisting to address width issues with media content display when zoomed in.  Updated manifest.json "browser_action" to "page_action"

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 
 const sidePanel = document.querySelector('.side');
-const siteTable = document.querySelector('#siteTable');
+const linklisting = document.querySelector('.linklisting');
 
+// fixes issue with some subs having threads that overlap the side panel
 const fixSiteTableWidth = () => {
-  siteTable.setAttribute('style', `max-width: ${window.innerWidth - sidePanel.offsetWidth - 48}px !important`)
+  linklisting.setAttribute('style', `max-width: ${window.innerWidth - sidePanel.offsetWidth - 48}px !important`)
 }
 
 fixSiteTableWidth();
