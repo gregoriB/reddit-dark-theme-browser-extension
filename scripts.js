023ab6f9ac580cf4fc5e79f8954f054e89636256
gregoriB <brandon.gregori@gmail.com>
2019-02-02 12:23:23 -0800

    replaced background of "my subreddits" drop down menu and background of thread sorting drop down  menu

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
-
 const sidePanel = document.querySelector('.side');
 const linklisting = document.querySelector('.linklisting');
 
 // fixes issue with some subs having threads that overlap the side panel
 const fixSiteTableWidth = () => {
-  linklisting.setAttribute('style', `max-width: ${window.innerWidth - sidePanel.offsetWidth - 48}px !important`)
+  linklisting.setAttribute('style', `max-width: ${window.innerWidth - sidePanel.offsetWidth - 48}px !important`);
 }
 
 fixSiteTableWidth();
 
-window.addEventListener('resize', fixSiteTableWidth)
+window.addEventListener('resize', fixSiteTableWidth);
